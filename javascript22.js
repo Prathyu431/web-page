eece67fe6e426ef1c8a941641686d6145d11cff4
Prathyu431 <prathyusha64252005@gmail.com>
2025-03-11 17:14:20 +0530

    Add files via upload

---
+++ javascript22.js
@@ -0,0 +1,14 @@
+
+let food3s = document.getElementById('food3');
+let food4s = document.getElementById('food4');
+let foods = document.getElementById('food');
+
+
+food3s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('food3.jpg')";
+
+})
+food4s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('food4.jpg')";
+
+})
